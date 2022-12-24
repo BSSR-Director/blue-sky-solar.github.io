aa68c99a21a2e7a93277242bb947a55c9211726c
hansonliu72888 <55341790+hansonliu72888@users.noreply.github.com>
2022-12-24 14:51:06 -0500

    12-24-4

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-
-$(document).ready(function() {
+$('body').click(function(e){    
     const hamburger = document.querySelector(".header-hamburger");
     const hamburgerDropdown = document.querySelector(".header-hamburger-dropdown");
-    hamburger.addEventListener("click", mobileMenu);
-    function mobileMenu() {
-        hamburger.classList.toggle("active");
-        hamburgerDropdown.classList.toggle("active");
+    if(e.target.classList == "header-hamburger"){
+        if(hamburger.classList.contains("active")){
+            hamburger.classList.remove("active");
+            hamburgerDropdown.classList.remove("active");
+        }
+        else{
+            hamburger.classList.add("active");
+            hamburgerDropdown.classList.add("active");
+        }
+        return;
+    }
+    if($(e.target).closest('.header-hamburger').length){
+        if(hamburger.classList.contains("active")){
+            hamburger.classList.remove("active");
+            hamburgerDropdown.classList.remove("active");
+        }
+        else{
+            hamburger.classList.add("active");
+            hamburgerDropdown.classList.add("active");
+        } 
+        return; 
+    }
+    if(hamburger.classList.contains("active")){
+        hamburger.classList.remove("active");
+        hamburgerDropdown.classList.remove("active");
+        return;
     }
 });
